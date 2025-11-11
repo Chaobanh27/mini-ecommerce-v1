6f3324ca1ad963286cb4ed8383b17a584b95171a
ChaoBanhNguyen <102232031+Chaobanh27@users.noreply.github.com>
2025-11-11 15:23:46 +0700

    update stripe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,33 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+// import { Route, Routes } from 'react-router-dom'
+// import Home from './pages/Home'
 
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe("pk_test_51NpwaQLViN3D9XNz99TnglqNN2pFfpj4hG2AXjscvtWZClEeMQXcRa61U8ljdjbhYaAjf1TfYM2VH4vXj2wrXPW7006j3z44QI");
 
 function App() {
+  const handleCheckout = async () => {
+    const res = await fetch("https://mini-ecommerce-v1.vercel.app/api/create-checkout-session", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        products: [{ name: "T-Shirt", price: 20, quantity: 1 }]
+      }),
+    });
 
+    const data = await res.json();
+    const stripe = await stripePromise;
+    await stripe.redirectToCheckout({ sessionId: data.id });
+  };
   return (
     <>
-    <Routes>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>T-Shirt - $20</h1>
+      <button onClick={handleCheckout}>Thanh toán</button>
+    </div>
+    {/* <Routes>
       <Route index element={<Home/>}/>
       <Route element />
-    </Routes>
+    </Routes> */}
     </>
   )
 }
